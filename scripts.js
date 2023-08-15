477165dbad2fb66e40165cc487c2ab3da44dfc02
Enrique <kike@Enriques-MacBook-Pro.local>
2023-08-14 21:06:20 -0600

    files and scaffoldings included 3

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@
 console.log('Hello WordPress')
 //To avoid jQuery version conflicts between WordPress and plugins
 $ = jQuery.noConflict()
-
 import {module1} from './js/module1'
 (()=>{
 	module1();
