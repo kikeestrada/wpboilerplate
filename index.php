89581ba6ef2a89224680b120ac4995fcab62c1b3
Enrique <kike@Enriques-MacBook-Pro.local>
2023-08-14 21:05:17 -0600

    files and scaffoldings included 2

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 get_header();
-get_template_part( 'content' );
+get_template_part( './template-parts/content' );
 get_footer();
--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-;
-console.log('Hello WordPress')
-//To avoid jQuery version conflicts between WordPress and plugins
-$ = jQuery.noConflict()
-
-import {module1} from './modules/module1'
-(()=>{
-	module1();
-})();
