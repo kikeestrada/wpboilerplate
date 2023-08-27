78df0a3dcc30b972666449216143b0c64e43e7a2
System Administrator <root@Enriques-MacBook-Pro.local>
2023-08-27 00:16:42 -0600

    permisos

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// ;
-// console.log('Hello WordPress')
-// import {module1} from './js/module1.js'
-// (()=>{
-// 	module1();
-// })();
-
-
-;
-console.log('Hello WordPress')
+(()=>{
+	console.log('Hello WordPress');
+})();
