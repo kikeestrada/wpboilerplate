1f18620d4c1df05d0e6145802f7ca20ee23ffdf5
altmktgdev <143544217+altmktgdev@users.noreply.github.com>
2023-11-02 09:57:48 -0700

    last update

--- core-functions/wpb_set-css-js.php
+++ core-functions/wpb_set-css-js.php
@@ -5,37 +5,41 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-// Encolar estilos y scripts personalizados del tema hijo
-function child_theme_enqueue_styles_and_scripts() {
-    // Desencolar estilos del tema padre
-    wp_dequeue_style('twenty-twenty-one-style');
-    wp_deregister_style('twenty-twenty-one-style');
+if ( !function_exists( 'child_theme_enqueue_styles_and_scripts' ) ) :
 
-    // Encolar CSS personalizado del tema hijo
-    $child_theme_css_path = get_stylesheet_directory() . '/theme-styles.css';
-    if (file_exists($child_theme_css_path)) {
-        wp_enqueue_style(
-            'child-theme-styles', // Handle
-            get_stylesheet_directory_uri() . '/theme-styles.css', // Ruta al archivo CSS del tema hijo
-            [], // Un array de estilos de los que depende
-            filemtime($child_theme_css_path) // Número de versión para el control de cache
-        );
+    function child_theme_enqueue_styles_and_scripts() {
+        // Desencolar estilos del tema padre
+        wp_dequeue_style('twenty-twenty-one-style');
+        wp_deregister_style('twenty-twenty-one-style');
+
+        // Encolar CSS personalizado del tema hijo
+        $child_theme_css_path = get_stylesheet_directory() . '/theme-styles.css';
+        if (file_exists($child_theme_css_path)) {
+            wp_enqueue_style(
+                'child-theme-styles', // Handle
+                get_stylesheet_directory_uri() . '/theme-styles.css', // Ruta al archivo CSS del tema hijo
+                [], // Un array de estilos de los que depende
+                filemtime($child_theme_css_path) // Número de versión para el control de cache
+            );
+        }
+
+        // Encolar JS personalizado del tema hijo
+        $child_theme_js_path = get_stylesheet_directory() . '/theme-scripts.js';
+        if (file_exists($child_theme_js_path)) {
+            wp_enqueue_script(
+                'child-theme-scripts', // Handle
+                get_stylesheet_directory_uri() . '/theme-scripts.js', // Ruta al archivo JS del tema hijo
+                ['jquery'], // Un array de scripts de los que depende
+                filemtime($child_theme_js_path), // Número de versión para el control de cache
+                true // Cargar en el footer
+            );
+        }
     }
 
-    // Encolar JS personalizado del tema hijo
-    $child_theme_js_path = get_stylesheet_directory() . '/theme-scripts.js';
-    if (file_exists($child_theme_js_path)) {
-        wp_enqueue_script(
-            'child-theme-scripts', // Handle
-            get_stylesheet_directory_uri() . '/theme-scripts.js', // Ruta al archivo JS del tema hijo
-            ['jquery'], // Un array de scripts de los que depende
-            filemtime($child_theme_js_path), // Número de versión para el control de cache
-            true // Cargar en el footer
-        );
-    }
-}
+endif;
 
 // Agregamos la acción con prioridad 20 para asegurarnos de que se ejecute después de que el tema padre haya encolado sus estilos
 add_action('wp_enqueue_scripts', 'child_theme_enqueue_styles_and_scripts', 20);
+
 
 ?>
--- js/theme-scripts.js
+++ js/theme-scripts.js
@@ -1,7 +1,7 @@
 ;
 console.log('Hello WordPress');
-import {module1} from './components/module1'
-import {module2} from './components/module2'
+import {module1} from './components/module1';
+import {module2} from './components/module2';
 (()=>{
 	// To avoid conflicts with jQuery versions between WordPress and its plugins
 	$ = jQuery.noConflict() 
